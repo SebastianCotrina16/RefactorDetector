f28f940850b50f89690b4182c57aab76769cc7fa
SebastianCotrina <sebastiancotrina16@gmail.com>
2023-10-21 12:33:49 -0500

    Remove mage

--- scripts/churn_calculator.py
+++ scripts/churn_calculator.py
@@ -14,6 +14,11 @@ class ChurnCalculator:
                 parts = line.split('\t')
                 if len(parts) >= 3:
                     additions, deletions, file_name = parts
+                    print(parts)
                     churn[file_name] = churn.get(file_name, 0) + int(additions) + int(deletions)
                 
         return churn
+if __name__ == '__main__':
+    churn = ChurnCalculator.get_churn()
+    for file_name, churn_value in churn.items():
+        print(f'Archivo: {file_name}, Churn: {churn_value}')
