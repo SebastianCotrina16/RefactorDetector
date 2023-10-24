43d962efa86f6a21308cbfe62e54bd819e91565d
SebastianCotrina <sebastiancotrina16@gmail.com>
2023-10-23 22:43:13 -0500

    Fixed display

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+from adjustText import adjust_text
 from complexity_analyzer import ComplexityAnalyzer
 from churn_calculator import ChurnCalculator
 
@@ -11,13 +12,18 @@ class Analyzer:
         x = [churn[file] for file in files]
         y = [complexities[file] for file in files]
 
-        plt.scatter(x, y)
+        plt.figure(figsize=(12, 8))
+
+        plt.scatter(x, y,alpha=0.6,edgecolors='w',linewidths=0.5)
+
         plt.xlabel('Churn (# Commits for each file)')
         plt.ylabel('Cyclomatic Complexity')
         plt.title('Churn vs Cyclomatic Complexity')
 
-        for file,churn_value,complexity in zip(files, x, y):
-            plt.annotate(file, (churn_value, complexity))
+        texts = [plt.text(churn_value, complexity, file, ha='center', va='center') for file, churn_value, complexity in zip(files, x, y)]
+        
+        adjust_text(texts)
+
         plt.savefig('complexity_vs_churn.png')
 
 if __name__ == '__main__':
